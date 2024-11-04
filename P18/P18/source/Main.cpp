3d12cedadf96e704883bb9e2222e44160bcbfd35
rayyu0826 <hyu32084@gmail.com>
2024-11-04 15:19:54 +0800

    first commit

---
+++ P18/P18/source/Main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char string1[60] = "Welcome";
+    char string2[60];
+
+    size_t len;
+
+    strcpy_s(string2, string1);
+    printf("string2=%s\n", string2);
+
+    len = strlen(string2);
+    printf("¦r¦êªø«×¬°%zd\n", len);
+
+    system("pause");
+    return 0;
+}
